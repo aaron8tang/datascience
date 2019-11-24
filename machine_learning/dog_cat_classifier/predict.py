497f1dfd7471b5f82e89d76972228bfe7cc26a7f
aaron8tang@qq.com <wordpa=1>
2019-11-25 01:27:03 +0800

    add acc plot

--- machine_learning/dog_cat_classifier/predict.py
+++ machine_learning/dog_cat_classifier/predict.py
@@ -6,74 +6,79 @@ from os.path import isfile, join
 
 
 def predict():
-    # dimensions of our images
-    img_width, img_height = 150, 150
+	# dimensions of our images
+	img_width, img_height = 150, 150
 
-    # load the model we saved
-    model = load_model('model.h5')
-    model.compile(loss='binary_crossentropy',
-                  optimizer='rmsprop',
-                  metrics=['accuracy'])
+	# load the model we saved
+	model = load_model('model.h5')
+	model.compile(loss='binary_crossentropy',
+				  optimizer='rmsprop',
+				  metrics=['accuracy'])
 
-    mypath = "predict/"
-    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-    print(onlyfiles)
-    # predicting images
-    dog_counter = 0
-    cat_counter  = 0
-    for file in onlyfiles:
-        img = image.load_img(mypath+file, target_size=(img_width, img_height))
-        x = image.img_to_array(img)
-        x = np.expand_dims(x, axis=0)
+	mypath = "predict/"
+	onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+	print(onlyfiles)
+	# predicting images
+	dog_counter = 0
+	cat_counter  = 0
+	for file in onlyfiles:
+		img = image.load_img(mypath+file, target_size=(img_width, img_height))
+		x = image.img_to_array(img)
+		x = np.expand_dims(x, axis=0)
 
-        images = np.vstack([x])
-        classes = model.predict_classes(images, batch_size=10)
-        classes = classes[0][0]
+		images = np.vstack([x])
+		classes = model.predict_classes(images, batch_size=10)
+		classes = classes[0][0]
 
-        if classes == 0:
-            print(file + ": " + 'cat')
-            cat_counter += 1
-        else:
-            print(file + ": " + 'dog')
-            dog_counter += 1
-    print("Total Dogs :",dog_counter)
-    print("Total Cats :",cat_counter)
+		if classes == 0:
+			print(file + ": " + 'cat')
+			cat_counter += 1
+		else:
+			print(file + ": " + 'dog')
+			dog_counter += 1
+	print("Total Dogs :",dog_counter)
+	print("Total Cats :",cat_counter)
 
 
 def predict2():
-    # dimensions of our images
-    img_width, img_height = 150, 150
+	# dimensions of our images
+	img_width, img_height = 150, 150
 
-    # load the model we saved
-    model = load_model('model.h5')
-    model.compile(loss='binary_crossentropy',
-                  optimizer='rmsprop',
-                  metrics=['accuracy'])
+	# load the model we saved
+	model = load_model('model.h5')
+	model.compile(loss='categorical_crossentropy',
+				  optimizer='rmsprop',
+				  metrics=['accuracy'])
 
-    mypath = "predict/"
-    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-    print(onlyfiles)
-    # predicting images
-    dog_counter = 0
-    cat_counter = 0
-    for file in onlyfiles:
-        img = image.load_img(mypath + file, target_size=(img_width, img_height))
-        x = image.img_to_array(img)
-        x = np.expand_dims(x, axis=0)
+	mypath = "predict/"
+	onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+	print(onlyfiles)
+	# predicting images
+	dog_counter = 0
+	cat_counter = 0
+	flower_counter = 0
+	for file in onlyfiles:
+		img = image.load_img(mypath + file, target_size=(img_width, img_height))
+		x = image.img_to_array(img)
+		x = np.expand_dims(x, axis=0)
 
-        images = np.vstack([x])
-        classes = model.predict_classes(images, batch_size=10)
-        #classes = classes[0][0]
+		images = np.vstack([x])
+		classes = model.predict_classes(images, batch_size=10)
+		#classes = classes[0][0]
 
-        if classes == 0:
-            print(file + ": " + 'cat')
-            cat_counter += 1
-        else:
-            print(file + ": " + 'dog')
-            dog_counter += 1
-    print("Total Dogs :", dog_counter)
-    print("Total Cats :", cat_counter)
+		if classes == 0:
+			print(file + ": " + 'cat')
+			cat_counter += 1
+		elif classes == 1:
+			print(file + ": " + 'dog')
+			dog_counter += 1
+		else:
+			print(file + ": " + 'flower')
+			flower_counter += 1
 
+	print("Total Dogs :", dog_counter)
+	print("Total Cats :", cat_counter)
+	print("Total Flowers :", flower_counter)
 
 if __name__ == '__main__':
-    predict2()
+	predict2()
--- machine_learning/dog_cat_classifier/train.py
+++ machine_learning/dog_cat_classifier/train.py
@@ -12,6 +12,8 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def buildModel():
@@ -96,7 +98,7 @@ def buildModel():
 
     print("Done in %f seconds." % (end - start))
 
-def buildModel2():
+def buildModel2(output):
     start = time()
 
     # dimensions of our images.
@@ -104,8 +106,8 @@ def buildModel2():
 
     train_data_dir = 'data/train'
     validation_data_dir = 'data/validation'
-    nb_train_samples = 2000
-    nb_validation_samples = 800
+    nb_train_samples = 1000
+    nb_validation_samples = 500
     epochs = 10
     batch_size = 16
 
@@ -117,24 +119,12 @@ def buildModel2():
     model = Sequential()
     model.add(Conv2D(32, (3, 3), input_shape=input_shape))
     model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Conv2D(32, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Conv2D(64, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Flatten())
-    model.add(Dense(64))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(2))
+    model.add(Dense(3))
     model.add(Activation('softmax'))
 
-    model.compile(loss='binary_crossentropy', # or categorical_crossentropy
+    model.compile(loss='categorical_crossentropy', # or categorical_crossentropy
                   optimizer='rmsprop',# or adagrad
                   metrics=['accuracy'])
 
@@ -163,7 +153,7 @@ def buildModel2():
         batch_size=batch_size,
         class_mode='categorical')
 
-    model.fit_generator(
+    H = model.fit_generator(
         train_generator,
         steps_per_epoch=nb_train_samples // batch_size,
         epochs=epochs,
@@ -172,6 +162,20 @@ def buildModel2():
 
     print(model.summary())
 
+    # Plot the training loss and accuracy
+    plt.style.use("ggplot")
+    plt.figure()
+    plt.plot(np.arange(0, epochs), H.history["loss"], label="train_loss")
+    plt.plot(np.arange(0, epochs), H.history["val_loss"], label="val_loss")
+    plt.plot(np.arange(0, epochs), H.history["accuracy"], label="train_acc")
+    plt.plot(np.arange(0, epochs), H.history["val_accuracy"], label="val_acc")
+    plt.title("Training Loss and Accuracy")
+    plt.xlabel("Epoch #")
+    plt.ylabel("Loss/Accuracy")
+    plt.legend()
+    plt.savefig(output)
+    plt.show()
+
     model.save('model.h5')
 
     end = time()
@@ -179,4 +183,6 @@ def buildModel2():
     print("Done in %f seconds." % (end - start))
 
 if __name__ == '__main__':
-    buildModel2()
+    output = 'c:/temp/train.png'
+
+    buildModel2(output)
